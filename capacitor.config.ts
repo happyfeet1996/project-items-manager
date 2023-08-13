824b61040ca8b75a73082888095693f57fb4b177
happy feet <40989006+happyfeet1996@users.noreply.github.com>
2023-08-13 18:18:07 +0800

    修改项目默认图标

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'project-items-manager',
+  appName: '每日待办',
   webDir: 'www',
   server: {
     androidScheme: 'https'
